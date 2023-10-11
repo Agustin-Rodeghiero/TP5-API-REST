6a5de54460d9a991bd08db0b544de14f1afe542b
Agustin-Rodeghiero <agustin.rodeghiero@gmail.com>
2023-10-11 08:40:59 -0300

    Initial Commit

--- src/main/java/com/utn/TPApiRest/Rodeghiero/TpApiRestRodeghieroApplication.java
+++ src/main/java/com/utn/TPApiRest/Rodeghiero/TpApiRestRodeghieroApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TpApiRestRodeghieroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TpApiRestRodeghieroApplication.class, args);
-		System.out.println("Esta corriendo la Api");
+		System.out.println("La Api esta funcionando");
 	}
 }
